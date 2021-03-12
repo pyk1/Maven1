60cf5632ec1f4292aed11083b1235be4b99daf03
pyk1 <arrav7@gmail.com>
2021-03-12 14:16:58 +0530

    src/ target/ pom.xml

---
+++ src/test/java/Dev/BadCase.java
@@ -0,0 +1,7 @@
+package Dev;
+
+class BadCase {
+    void method() {
+        System.out.println("Hello SpotBugs!");
+    }
+}
---
+++ src/test/java/Dev/GoodCase.java
@@ -0,0 +1,7 @@
+package Dev;
+
+class GoodCase {
+    void method() {
+        System.err.println("Hello SpotBugs!");
+    }
+}
